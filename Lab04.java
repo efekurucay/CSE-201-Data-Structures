689d431a770f0dfde2753c0ff22f80330e7b4803
Yahya Efe Kuruçay <109385388+efekurucay@users.noreply.github.com>
2024-10-10 13:40:41 +0300

    update

---
+++ Lab04.java
@@ -0,0 +1,75 @@
+public class Lab04 {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+     * 1. Write a recursive method 'static int factorial(int n)' 
+     * that returns the factorial of given n
+     */
+
+    /*
+     * 2. Write a recursive method 'static double power(double base, int exponent)'
+     *  to compute the result of raising base to the power of exponent
+     */
+
+     /*
+     * 3. Write a recursive method 'static boolean isPalindrome(String s)' 
+     *  that checks if a given string is palindrome
+     */
+
+     /*
+     * 4. Write a recursive method 
+     * 'static int binarySearch(int[] arr, int target, int low, int high)'
+     *  to find the index of target in a sorted array
+     */
+
+     /*
+     * 5.1. Write a recursive method 'static int fibonacci(int n)'
+     *  that returns the nth fibonacci number
+     *  fibonacci(5) should return 3: sequence(0, 1, 1, 2, 3, 5)
+     */
+
+     /*
+     * 5.2. Write a recursive method 'static int betterFibonacci(int n, int[] sequence)'
+     *  that returns the nth fibonacci number
+     *  betterFibonacci(5) should return 3: sequence(0, 1, 1, 2, 3, 5)
+     */
+
+     /*
+     * 5.3. Investigate the performance difference between 5.1 and 5.2
+     */
+
+    /*
+     * 6. Write a recursive method 'static void generatePermutations(String str, string prefix)'
+     *  that prints all permutations
+     * geneatePermutations("abc", "") should print abc, acb, bac, bca, cab, cba
+     */
+
+
+    /*
+     * 7. Write a recursive method 'static boolean subsetSum(int[] array, int n, int sum)'
+     *  that determines if there exists a subset of the array  that adds up to sum
+     * subsetSum(new int[]{1, 2, 3, 4}, 4, 5) should return true because 1 + 4 = 5
+     */
+
+    /*
+     * 8. Write a recursive method List<List<Integer>> getSubsequences(int[] array, int n, List<Integer> current)
+     *  that returns all subsequences of an array.
+     * getSubsequences(new int[]{1, 2, 3}, 0, new ArrayList<>()) should return list similar to
+     * [[], [3], [2], [2, 3], [1], [1, 3], [1, 2], [1, 2, 3]]
+     */
+
+     /*
+      * 9. Implement a basic singly linked list
+      * 9.1. Implement a Node class
+      * 9.1.1 Node class with only public data and next fields
+      * 9.1.2. should have a toString() method to test your code
+      * 9.2. Implement a SinglyLinkedList class
+      * 9.2.1. SinglyLinkedList class should only have a public head field
+      * 9.2.2. void addLast(T data)
+      * 9.2.3. Write a recursive method Node<T> reverse(Node<T> node)
+      * which reverses a linked list with its given head
+      * 9.2.4. should have a toString() method to test your code
+      */
+}
